a8eaa5658851aa16e362048e09d9d3e073b2c3ee
Murcilton <tutstuts.belka@mail.ru>
2024-11-25 21:25:31 +0500

    carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        animateOut: 'fadeOut',
+        nav: true, 
+        dots: true, 
+        smartSpeed: 650
+    });
+});
