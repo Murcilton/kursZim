2c39ad2b83eb912e8dc929eb0a2f7fb5126125db
Murcilton <tutstuts.belka@mail.ru>
2024-11-25 17:54:32 +0500

    1

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,30 @@
+<div class="header">
+    <img src="{{ url('storage/GUI/Header Background.svg') }}" alt="" title="" style="width: 100%; position: absolute; "/>
+    <div class="headernav row align-items-center">
+        <div class="col">
+            <button class="btn logo" style=" "><img src="{{ url('storage/GUI/Logo.svg') }}" class="" alt="" title="" style="width: 130px"/></button>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <button class="btn" style="position: relative; "><img src="{{ url('storage/GUI/Burger Menu.svg') }}" class="burgerMenuButton" alt="" title=""/></button>
+        </div>
+        <div class="col">
+            <button class="btn btn-primary btnnav" style="position: relative; ">КРУИЗЫ</button>
+        </div>
+        <div class="col">
+            <button class="btn btn-primary btnnav" style="position: relative; ">КОРАБЛИ</button>
+        </div>
+        <div class="col">
+            <button class="btn btn-primary btnnav" style="position: relative; ">ПРИБЫТИЯ</button>
+        </div>
+        <div class="col">
+            <button class="btn btn-primary btnnav" style="position: relative; width: 240px">СОЗДАТЬ СВОЙ КРУИЗ</button>
+        </div>
+        <div class="col">
+            <button class="btn d-flex justify-content-center" style="position: relative; "><img src="{{ url('storage/GUI/Search Button.svg') }}" class="SearchButton" alt="" title=""/></button>
+        </div>
+        <div class="col">
+            <button class="btn btn-primary btnnav" style="position: relative; ">Кабинет</button>
+        </div>
+    </div>
+</div>
+
