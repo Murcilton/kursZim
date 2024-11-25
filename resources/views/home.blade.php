5bc7851a36319aa371b8036c57a667f79e6f1e78
Murcilton <tutstuts.belka@mail.ru>
2024-11-25 20:11:17 +0500

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.layout')
 
 @section('content')
-
+{{-- <svg width="1080" height="456" viewBox="0 0 1080 456" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M1079 0H0V438C0 447.941 241.766 456 540 456C838.234 456 1080 447.941 1080 438C1080 437.629 1079.66 437.261 1079 436.895V0Z" fill="#012840" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M1079 0H0V438C0 447.941 241.766 456 540 456C838.234 456 1080 447.941 1080 438C1080 437.629 1079.66 437.261 1079 436.895V0Z" fill="url(#pattern0_11_34)" />
+    <defs>
+      <pattern id="pattern0_11_34" patternContentUnits="objectBoundingBox" width="1" height="1">
+        <use xlink:href="#image0_11_34" transform="matrix(0.000625 0 0 0.00148026 0 -0.166118)" />
+        <image xlink:href="{{ asset('storage/img/Union.png') }}" width="1600" height="900" transform="matrix(0.000625 0 0 0.00148026 0 -0.166118)" />  
+    </pattern>
+    </defs>
+  </svg> --}}
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,30 +1,15 @@
-<div class="header">
-    <img src="{{ url('storage/GUI/Header Background.svg') }}" alt="" title="" style="width: 100%; position: absolute; "/>
-    <div class="headernav row align-items-center">
-        <div class="col">
-            <button class="btn logo" style=" "><img src="{{ url('storage/GUI/Logo.svg') }}" class="" alt="" title="" style="width: 130px"/></button>
-        </div>
-        <div class="col d-flex justify-content-center">
-            <button class="btn" style="position: relative; "><img src="{{ url('storage/GUI/Burger Menu.svg') }}" class="burgerMenuButton" alt="" title=""/></button>
-        </div>
-        <div class="col">
-            <button class="btn btn-primary btnnav" style="position: relative; ">КРУИЗЫ</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-primary btnnav" style="position: relative; ">КОРАБЛИ</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-primary btnnav" style="position: relative; ">ПРИБЫТИЯ</button>
-        </div>
-        <div class="col">
-            <button class="btn btn-primary btnnav" style="position: relative; width: 240px">СОЗДАТЬ СВОЙ КРУИЗ</button>
-        </div>
-        <div class="col">
-            <button class="btn d-flex justify-content-center" style="position: relative; "><img src="{{ url('storage/GUI/Search Button.svg') }}" class="SearchButton" alt="" title=""/></button>
-        </div>
-        <div class="col">
-            <button class="btn btn-primary btnnav" style="position: relative; ">Кабинет</button>
-        </div>
-    </div>
+<div class="header"> 
+    <img class="hbackground" src="{{ url('storage/GUI/Header Background.svg') }}" alt="" title="" style=""/> 
+    <div class="headernav d-flex align-items-center"> 
+        <div class="logo-container">
+            <button class="btn logo"><img src="{{ url('storage/GUI/Logo.svg') }}" alt="" title="" style="width: 130px"/></button> 
+        </div> 
+        <button class="btn burger-button"><img src="{{ url('storage/GUI/Burger Menu.svg') }}" class="burgerMenuButton" alt="" title=""/></button> 
+        <button class="btn btn-primary btnnav">КРУИЗЫ</button> 
+        <button class="btn btn-primary btnnav">КОРАБЛИ</button> 
+        <button class="btn btn-primary btnnav">ПРИБЫТИЯ</button> 
+        <button class="btn btn-primary btnnav create-cruise-btn">СОЗДАТЬ СВОЙ КРУИЗ</button> 
+        <button class="btn search-button"><img src="{{ url('storage/GUI/Search Button.svg') }}" class="SearchButton" alt="" title=""/></button> 
+        <button class="btn btn-primary btnnav cabinetButton" style="">Кабинет</button> 
+    </div> 
 </div>
-
